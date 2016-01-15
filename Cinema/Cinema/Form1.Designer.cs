e549bfd390ee7e9e05edba65fd1c66ecdc078978
Pavlo Khryshcheniuk <wik2006@ukr.net>
2016-01-15 21:18:44 +0100

    fff

--- Cinema/Cinema/Form1.Designer.cs
+++ Cinema/Cinema/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(100, 145);
+            this.button1.Location = new System.Drawing.Point(100, 167);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -43,7 +43,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(100, 174);
+            this.button2.Location = new System.Drawing.Point(100, 196);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
