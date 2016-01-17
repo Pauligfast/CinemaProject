f2c76e27462b2c14134a8962d57014892dd59b7d
Pavlo Khryshcheniuk <wik2006@ukr.net>
2016-01-17 13:46:02 +0100

    jnkn

---
+++ Cinema/Cinema/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Cinema")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cinema")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("a8a53184-e94b-4ced-b91e-c7323793a60d")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ Cinema/Cinema/UserForm.cs
@@ -0,0 +1,35 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: Cinema.UserForm
+// Assembly: Cinema, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: 93C9DD96-8ED5-40E1-AA89-2D6DD39249D6
+// Assembly location: C:\Users\Paulig\Source\Repos\CinemaProject\Cinema\Cinema\obj\Debug\Cinema.exe
+
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace Cinema
+{
+  public class UserForm : Form
+  {
+    private IContainer components = (IContainer) null;
+
+    public UserForm()
+    {
+      this.InitializeComponent();
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && this.components != null)
+        this.components.Dispose();
+      base.Dispose(disposing);
+    }
+
+    private void InitializeComponent()
+    {
+      this.components = (IContainer) new Container();
+      this.AutoScaleMode = AutoScaleMode.Font;
+      this.Text = "UserForm";
+    }
+  }
+}
